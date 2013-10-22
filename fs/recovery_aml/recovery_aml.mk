cb43b11b0c9901dfe65e1947b69030e2b16f9ebd
J1nx <admin@j1nx.nl>
2013-10-22 19:28:17 +0000

    [TLBB] Implement recovery flash into update.zip

--- fs/recovery_aml/recovery_aml.mk
+++ fs/recovery_aml/recovery_aml.mk
@@ -26,7 +26,7 @@ define ROOTFS_RECOVERY_AML_CMD
  find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
  find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
  pushd $(BINARIES_DIR)/aml_recovery/ >/dev/null && \
- zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/update-unsigned.zip aml_logo.img uImage-2.6.34.14 META-INF system && \
+ zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/update-unsigned.zip aml_logo.img uImage-2.6.34.14 uImage-recovery META-INF system && \
  popd >/dev/null && \
  echo "Signing update.zip..." && \
  pushd fs/recovery_aml/ >/dev/null; java -Xmx1024m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $(BINARIES_DIR)/aml_recovery/update-unsigned.zip $(BINARIES_DIR)/update.zip && \
